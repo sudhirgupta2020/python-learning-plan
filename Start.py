1f36ff2d1bf239dffe257fe30de20dc8fbdea686
sudhirgupta2020 <70596577+sudhirgupta2020@users.noreply.github.com>
2020-12-21 18:38:12 +0545

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,1 @@
+print("Sudhir Kumar Gupta")
---
+++ star.py
@@ -0,0 +1,21 @@
+#price = 490
+
+#if price > 500:
+   #print("I will not buy")
+#else:
+   #print("I will present as a gift")
+   #ages = [13,17,20,24,28]
+
+   #for age in ages:
+       #print("this friend age:", age)
+#for num in range(0,10):
+    #print(num)
+num = 0
+while num < 10:
+    print(num)
+    num = num + 1   
+
+def brushTeeth():
+    print("Brushing")  
+
+brushTeeth()          
